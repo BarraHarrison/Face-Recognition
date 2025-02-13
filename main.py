c79d37b42e990af386af81cdeb64b67e834ef587
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-13 17:14:46 +0900

    Setting the frame width & height

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Python Program for Face Recognition
+import threading
+import cv2
+from deepface import DeepFace
+
+capture_object = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+capture_object.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture_object.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
