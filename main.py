04bd6427934603e45b56fb1e9f0aea8f061ddd56
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-13 18:09:00 +0900

    Main function initializes video capture

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import threading
 import cv2
 from deepface import DeepFace
 
-capture_object = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-capture_object.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-capture_object.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
 
 
 def verify_face(frame, reference_image, result_container):
@@ -19,27 +17,10 @@ def verify_face(frame, reference_image, result_container):
     except ValueError:
         result_container["face_match"] = False
 
-# while True:
-#     ret, frame = capture_object.read()
-    
-#     if ret:
-#         if counter % 30 == 0:
-#             try:
-#                 threading.Thread(target=check_face, args=(frame.copy(),)).start()
-#             except ValueError:
-#                 pass
-        
-#         counter += 1
 
-#         if face_match:
-#             cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-#         else:
-#             cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+def __main__():
+    capture_object = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    capture_object.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    capture_object.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-#         cv2.imshow("Result", frame)
-    
-#     key = cv2.waitKey(1)
-#     if key == ord("q"):
-#         break
 
-# cv2.destroyAllWindows()
