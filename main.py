41f24749a81434aaaeaf2d34ce469d33d3d77bfc
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-13 17:32:19 +0900

    Text colour changes to red if there is no match

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ while True:
 
         if face_match:
             cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
     
     key = cv2.waitKey(1)
     if key == ord("q"):
