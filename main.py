4170e8dcddb414609b8169b3f25177310ff57774
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-13 17:17:11 +0900

    Creating counter variable and Boolean set to false

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ capture_object = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 capture_object.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture_object.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+counter = 0
+
+face_match = False
