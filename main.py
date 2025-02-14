4fe8538cea04e57f4d946757f69972e8eff4bd8f
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-02-14 23:16:44 +0900

    load_weights method seems to be depreciated

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from deepface import DeepFace
 current_dir = os.path.dirname(os.path.abspath(__file__))
 weights_path = os.path.join(current_dir, 'vgg_face_weights.h5')
 
-DeepFace.build_model("VGG-Face")
-DeepFace.load_weights("VGG-Face", weights_path)
+DeepFace.build_model("VGG-Face", model_path=weights_path)
 
 def verify_face(frame, reference_image, result_container):
     """
